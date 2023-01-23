96701f6564b9670eda174f29863aab22b4991aa4
emadruida <emadruida@gmail.com>
2023-01-23 13:23:59 +0100

    Bootstrap ng app on electron

--- main.js
+++ main.js
@@ -4,13 +4,10 @@ const path = require('path');
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
+        height: 600
     });
 
-    win.loadFile('index.html');
+    win.loadFile('ng/dist/ng/index.html');
 };
 
 app.whenReady().then(() => {
