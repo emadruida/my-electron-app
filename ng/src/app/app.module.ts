b4df6534be7a8f0164de983613680f98923a25b6
emadruida <emadruida@gmail.com>
2024-05-24 10:09:03 +0200

    Update angular version to 18.0.1

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,10 +17,11 @@ import { ListadoComponent } from './listado/listado.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
