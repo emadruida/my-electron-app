6fcc6e1118ebadf047819c140398c89b7628906e
emadruida <emadruida@gmail.com>
2025-05-29 08:50:01 +0200

    Update angular to version 20.0.0

--- ng/src/app/app.config.ts
+++ ng/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 
 export const appConfig : ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
   ]
